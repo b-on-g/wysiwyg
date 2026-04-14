657330303b5fc37c85546906f0aa6ca07979a80e
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-04-14 16:27:58 +0300

    Fix: padding '0' → 0 in permissions styles

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -165,9 +165,9 @@ namespace $ {
 			},
 			padding: {
 				top: '0.5rem',
-				bottom: '0',
-				left: '0',
-				right: '0',
+				bottom: 0,
+				left: 0,
+				right: 0,
 			},
 			border: {
 				top: {
