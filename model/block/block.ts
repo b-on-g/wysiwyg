cead86a6acbb859218d42d9e091a77815e232aff
novgorodcev.k <cmyser.fast.i@gmail.com>
2026-08-01 03:08:01 +0300

    Тип блока обычным текстом вместо атома

--- model/block/block.ts
+++ model/block/block.ts
@@ -1,7 +1,7 @@
 namespace $.$$ {
 	/** A single block in a WYSIWYG page. Stored as a pawn inside the page land. */
 	export class $bog_wysiwyg_model_block extends $giper_baza_dict.with({
-		Type: $giper_baza_atom_text,
+		Type: $giper_baza_text,
 		Level: $giper_baza_atom_real,
 		Content: $giper_baza_atom_text,
 	}) {}
